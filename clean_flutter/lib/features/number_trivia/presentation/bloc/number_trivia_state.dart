e6534a93ed65900f3311ac1d2f4041f71655c2b5
Bruno Azenha Gonçalves <azenha.bruno@gmail.com>
2020-06-10 19:33:44 +0200

    Complete chapter 10 Clean Flutter

---
+++ clean_flutter/lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -0,0 +1,33 @@
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaState extends Equatable {
+  const NumberTriviaState();
+}
+
+class Empty extends NumberTriviaState {
+  @override
+  List<Object> get props => [];
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  List<Object> get props => [];
+}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia});
+
+  @override
+  List<Object> get props => [trivia];
+}
+
+class Error extends NumberTriviaState {
+  final String errorMessage;
+
+  Error({@required this.errorMessage});
+
+  @override
+  List<Object> get props => [errorMessage];
+}
